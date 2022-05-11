3111350e341bec4455f89c56348f76caecf6afc6
aquif <aquif@lal10.com>
2022-05-11 22:15:21 +0530

    fix : fix issue in maximum array sum

--- array/maximum_sub_array/solution.js
+++ array/maximum_sub_array/solution.js
@@ -1,16 +1,17 @@
 // Maximum Subarray sum
 
-const maximumSubArray = (arr) => {
-    let max = 0;
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-        sum += arr[i];
-        if (sum > max)  max = sum;
-        if(sum < 0) sum = 0;
-    }
+const maximumSubArray = (nums) => {
+    let max1 = nums[0];
+    let max2 = nums[0];
 
-    return max;
+    for(let i=1; i<nums.length; i++){
+        max1 = Math.max(nums[i] , max1 +nums[i])
+
+        if(max1 > max2) max2 = max1;
+    }
+    
+    return max2;
 }
 
-let array = [-2,1,-3,4,-1,2,1,-5,4]
+let array = [5,4,-1,7,8]
 console.log(maximumSubArray(array));
